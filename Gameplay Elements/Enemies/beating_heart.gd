b5decfac5b7a068eed369174054eb9fd46ccd6eb
Jorge A <Jarrazola1a@gmail.com>
2025-12-23 14:43:46 -0600

    Heartbreak animations
    wit no 808s
    
    added 3 animations that are played on the health indicator, alternating randomly when dealing damage to an enemy

--- Gameplay Elements/Enemies/beating_heart.gd
+++ Gameplay Elements/Enemies/beating_heart.gd
@@ -1,4 +1,8 @@
 extends AnimatedSprite2D
+var breakAnims = ["BreakA","BreakB","BreakC"]
 func die():
+	set_animation(breakAnims[randi_range(0,2)])
+
+func _on_animation_finished():
 	Globals.debrisList.erase(self)
 	queue_free()
